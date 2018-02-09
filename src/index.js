bb40af302299ab3e16cae666f06214059f64cb6f
Mohamed El Mahallawy <mmahalwy@gmail.com>
2018-02-09 01:01:40 -0800

    forgot to do aggregation

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import '@babel/polyfill';
 
 export { default as BaseExchange } from './exchanges/base/BaseExchange';
+export { default as Aggregation } from './Aggregation';
 
 export { default as Binance } from './exchanges/Binance';
 export { default as Gdax } from './exchanges/Gdax';
